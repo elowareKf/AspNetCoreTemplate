e69be816b342d8ac5c36b06e18ba6747b0f571c6
KFischer (MBP) <klaus.fischer@eloware.com>
2020-05-02 14:57:07 +0200

    Added User decoder

--- Template.Server/Authentication/ILogonService.cs
+++ Template.Server/Authentication/ILogonService.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Template.Models;
+
 namespace Template.Server.Authentication {
     public interface ILogonService {
         JwtUser Authenticate(string username, string password, string deviceToken = null);
+        public User DecodeToken(HttpRequest request);
+        public User DecodeToken(string token);
+
     }
 }
--- Template.Server/Authentication/LogonService.cs
+++ Template.Server/Authentication/LogonService.cs
@@ -3,8 +3,10 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using Template.Db;
+using Template.Models;
 using Template.Server.DependencyInjection;
 
 namespace Template.Server.Authentication {
@@ -28,7 +30,19 @@ namespace Template.Server.Authentication {
             result.Token = CreateToken(result);
             return result;
         }
+        public User DecodeToken(HttpRequest request)
+            => DecodeToken(request.Headers["Authorization"].ToString());
+        
+        public User DecodeToken(string token) {
+            var jwt = new JwtSecurityToken(token);
+            if (jwt.ValidFrom > DateTime.Now || jwt.ValidTo < DateTime.Now)
+                return null;
 
+            var id = jwt.Claims.FirstOrDefault(c=>c.Type == "Id")?.Value;
+            return _unitOfWork.Users.Get(id);
+        }
+
+        /*
         public JwtUser DecodeToken(string token) {
             var jwt = new JwtSecurityToken(token);
             if (jwt.ValidFrom > DateTime.Now || jwt.ValidTo < DateTime.Now)
@@ -36,6 +50,7 @@ namespace Template.Server.Authentication {
 
             return new JwtUser(jwt.Claims);
         }
+        */
 
         public string CreateToken(JwtUser user) {
             var tokenHandler = new JwtSecurityTokenHandler();
